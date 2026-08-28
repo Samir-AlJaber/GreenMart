cd3c623e6cd4d3e8c3e8458ca0157125219c3d05
Tanimm27 <ktanim647@gmail.com>
2026-08-28 22:04:18 +0600

    Login page added

---
+++ application/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GreenMart.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
