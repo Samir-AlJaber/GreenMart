25b16d8174b21a13975003f16b44c6ead65d9a93
Raiyan <fahim.cse.20220204098@aust.edu>
2026-08-29 21:38:20 +0600

    Register page and Navbar updated

--- application/Models/ProfileUpdateModel.cs
+++ application/Models/ProfileUpdateModel.cs
@@ -4,31 +4,44 @@ namespace GreenMart.Models
 {
     public class ProfileUpdateModel
     {
-        public string CurrentEmail { get; set; }
+
+        public string? CurrentEmail { get; set; }
 
 
-        [Required]
+
+        [Required(ErrorMessage = "Name cannot be empty")]
         public string FullName { get; set; }
 
 
-        [EmailAddress]
+
+        [Required(ErrorMessage = "Email cannot be empty")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
 
 
-        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Phone number cannot be empty")]
+        [RegularExpression(
+            @"^\d{11}$",
+            ErrorMessage = "Phone number must contain exactly 11 digits"
+        )]
+        public string PhoneNumber { get; set; }
+
 
 
         public string? Address { get; set; }
 
 
 
-        public string CurrentPassword { get; set; }
+        public string? CurrentPassword { get; set; }
 
 
-        public string NewPassword { get; set; }
+
+        public string? NewPassword { get; set; }
 
 
-        public string ConfirmPassword { get; set; }
+
+        public string? ConfirmPassword { get; set; }
 
     }
 }
